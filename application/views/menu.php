634f0cbbd8918a0ec86edd91fff91c5286930c76
Evelyn Campos <elenuco36@gmail.com>
2020-02-29 00:13:22 -0600

    rama.
    creoesta rama para aislar el error

--- application/views/menu.php
+++ application/views/menu.php
@@ -37,7 +37,7 @@
 			<li>
 				<a href="javascript:void(0)" aria-expanded="true"><i class="fas fa-user-md"></i><span><p>MENU principal</p></span></a>
 				<ul class="collapse">
-					<li> <a href="<?php echo base_url(); ?>principal_controller/users">Mantenmiento de Usuarios</a> </li>
+					<li> <a href="/<?php echo base_url(); ?>principal_controller/index">Mantenmiento de Usuarios</a> </li>
 					<li> <a href="<?php echo base_url(); ?>principal_controller/">mantenimiento de roles</a> </li>
 					<li> <a href="<?php echo base_url(); ?>principal_controller/">Mantenimiento de permisos</a> </li>
 				</ul>
